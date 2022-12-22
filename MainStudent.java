d9e93321e943a42c49e239b22d413c07409bf1f2
SinchanaJ6 <121023449+SinchanaJ6@users.noreply.github.com>
2022-12-22 13:24:37 +0530

    Add files via upload

---
+++ MainStudent.java
@@ -0,0 +1,44 @@
+package com.student;
+
+import java.util.Scanner;
+
+public class MainStudent {
+  public static void main(String[] args) {
+	  Scanner sc=new Scanner(System.in);
+	  Studentmgtimpl s=new Studentmgtimpl();
+	while(true)
+	{
+		
+		System.out.println("enter your choice");
+		int i=sc.nextInt();
+		switch(i) {
+		case 1:
+			s.addStudent();
+			break;
+		case 2:
+			String id=sc.next();
+			s.displayStudent(id);
+			break;
+		case 3:
+			s.dispalyallStudent();
+			break;
+		case 4:
+			String id1=sc.next();
+			s.removeStudent(id1);
+			break;
+		case 5:
+			s.removeallStudent();
+			break;
+		case 6:
+			s.updateStudent();
+			break;
+		case 7:
+			s.sortStudent();
+			break;
+		default :
+			System.out.println("invalid choice");
+		}
+	}
+}
+  
+}
